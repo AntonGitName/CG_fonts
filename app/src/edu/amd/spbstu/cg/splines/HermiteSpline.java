7a0967c9a306c3535f8658f0d275beb5dede06ce
AShagal <aleksandra.shagal@gmail.com>
2015-03-15 16:48:36 +0300

    Added line drawing

--- app/src/edu/amd/spbstu/cg/splines/HermiteSpline.java
+++ app/src/edu/amd/spbstu/cg/splines/HermiteSpline.java
@@ -1,45 +1,63 @@
 package edu.amd.spbstu.cg.splines;
 
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 /**
  * @author Aleksandra
  * @since 14.03.2015.
  */
 public class HermiteSpline {
-    private void findQ(float q0, float qm, float[] p, int m, float[] q) {
-        final float[] a = new float[m];
-        final float[] b = new float[m];
-        a[1] = 0.f;
-        b[1] = q0;
-        q[m] = qm;
+    private static void findQ(Point q0, Point qm, List<Point> p, float[] qx, float[] qy) {
+        final int m = p.size() - 1;
+        final float[] ax = new float[m + 1];
+        final float[] bx = new float[m + 1];
+        final float[] ay = new float[m + 1];
+        final float[] by = new float[m + 1];
+        ax[1] = 0.f;
+        bx[1] = q0.x;
+        qx[m] = qm.x;
+
+        ay[1] = 0.f;
+        by[1] = q0.y;
+        qy[m] = qm.y;
         for (int i = 1; i < m; ++i) {
-            a[i + 1] = -0.25f + a[i];
-            b[i + 1] = (b[i] - 3 * (p[i + 1] - p[i - 1])) * a[i + 1];
+            ax[i + 1] = -0.25f + ax[i];
+            bx[i + 1] = (bx[i] - 3 * (p.get(i + 1).x - p.get(i - 1).x)) * ax[i + 1];
+            ay[i + 1] = -0.25f + ay[i];
+            by[i + 1] = (by[i] - 3 * (p.get(i + 1).y - p.get(i - 1).y)) * ay[i + 1];
         }
-        for (int i = m - 1; i >= 0; ++i) {
-            q[i] = a[i + 1] * q[i + 1] + b[i + 1];
+        for (int i = m - 1; i >= 0; --i) {
+            qx[i] = ax[i + 1] * qx[i + 1] + bx[i + 1];
+            qy[i] = ay[i + 1] * qy[i + 1] + by[i + 1];
         }
     }
 
-    private float hermiteVal(float p1, float p2, float q1, float q2, float t) {
+    private static float hermiteVal(float p1, float p2, float q1, float q2, float t) {
         final float t2 = t * t;
         final float t3 = t2 * t;
 
         return (1.f - 3 * t2 + 2 * t3) * p1 + t2 * (3.f - 2 * t) * p2 + t * (1.f - 2 * t + t2) * q1 - t2 * (t - 1.f) * q2;
     }
 
-    public void hermite(float[] p, int m, float q0, float qm, int div, float[] out) {
+    public static List<Point> hermite(List<Point> p, Point q0, Point qm, int div) {
         float t, dt = 1.f / div;
-        final float[] q = new float[m];
-        int start = 0;
-
-        findQ(q0, qm, p, m - 1, q);
+        final int m = p.size();
+        final float[] qx = new float[m];
+        final float[] qy = new float[m];
+        List<Point> out = new ArrayList<>();
+        findQ(q0, qm, p, qx, qy);
         for (int i = 1; i < m; ++i) {
             t = 0.f;
             for (int j = 0; j <= div; ++j) {
-                out[start + j] = hermiteVal(p[i - 1], p[i], q[i - 1], q[i], t);
+                Point point = new Point();
+                point.x = (int) hermiteVal(p.get(i - 1).x, p.get(i).x, qx[i - 1], qx[i], t);
+                point.y = (int) hermiteVal(p.get(i - 1).y, p.get(i).y, qy[i - 1], qy[i], t);
+                out.add(point);
                 t += dt;
             }
-            start += div;
         }
+        return out;
     }
 }
--- app/src/edu/amd/spbstu/cg/ui/PaintArea.java
+++ app/src/edu/amd/spbstu/cg/ui/PaintArea.java
@@ -1,10 +1,106 @@
 package edu.amd.spbstu.cg.ui;
 
-import javax.swing.*;
+import edu.amd.spbstu.cg.splines.HermiteSpline;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
 /**
  * @author iAnton
  * @since 15/03/15
  */
 public class PaintArea extends JPanel {
+    // Point to hold the mouse cursor's location
+    private List<Point> v = new ArrayList<>();
+
+    // constants for the drawn circle
+    private final Color DRAW_COLOR = Color.RED;
+    private final int DRAW_DIAMETER = 15;
+    private List<Point> line = new ArrayList<>();
+
+    // instance variables for the circle
+    private Color drawColor;
+    private int drawDiameter;
+
+    // constructor
+    public PaintArea() {
+        drawColor = DRAW_COLOR;
+        drawDiameter = DRAW_DIAMETER;
+        addMouseListener(
+
+                new MouseListener() // anonymous inner class
+                {
+                    // event handler called when a mouse button is clicked
+                    public void mouseClicked(MouseEvent event) {
+                    }
+
+                    // event handler called when mouse enters this DrawJPanel
+                    public void mouseEntered(MouseEvent event) {
+                    }
+
+                    // event handler called when mouse exits this DrawJPanel
+                    public void mouseExited(MouseEvent event) {
+                    }
+
+                    // event handler called when a mouse button is pressed
+                    public void mousePressed(MouseEvent event) {
+                        drawJPanelMousePressed(event);
+                    }
+
+                    // event handler called when a mouse button is released
+                    public void mouseReleased(MouseEvent event) {
+                    }
+
+                } // end anonymous inner class
+
+        ); // end call to addMouseListener
+
+    }  // end constructor
+
+    // draw a circle on this DrawJPanel
+    private void drawJPanelMousePressed(MouseEvent event) {
+        // store the location of the mouse
+        Point p = event.getPoint();
+
+        if (event.isMetaDown()) // right mouse button is pressed
+        {
+
+        } else // left mouse button is pressed
+        {
+            v.add(p);
+            if (v.size() > 5) {
+                Point q0 = new Point();
+                Point qm = new Point();
+                q0.x = -1;
+                q0.y = 1;
+                qm.x = 1;
+                qm.y = 0;
+                line = HermiteSpline.hermite(v, q0, qm, 10);
+            }
+        }
+
+        repaint(); // repaint this DrawJPanel
+
+    } // end method drawJPanelMousePressed
+
+    // draw a small circle at the mouse's location
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(drawColor); // set the color
+
+        for (Point p : v) {
+            // draw a filled circle at the mouse's location
+            g.drawOval(p.x - drawDiameter / 2, p.y - drawDiameter / 2, drawDiameter, drawDiameter);
+        }
+        if (v.size() > 5) {
+            for (int i = 0; i < line.size() - 1; ++i) {
+                g.drawLine(line.get(i).x, line.get(i).y, line.get(i + 1).x, line.get(i + 1).y);
+            }
+        }
+
+    } // end method paintComponent
+
 }
